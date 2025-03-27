b21a7eb52c6f0921b5f223980c1374747cec1ad0
kpstorgaard <s204130@dtu.dk>
2025-03-27 16:25:50 +0100

    Adding plot_data function to __init__ and main

--- examples/main.py
+++ examples/main.py
@@ -2,9 +2,22 @@
 import sys
 from pathlib import Path
 sys.path.append(str(Path(__file__).resolve().parents[1]))
-
 import src as src 
 
-data_dir = './inputs/Location1.csv'
+# Can be modified for the different sites
+site_index = 3
+
+# Load the data for the specified site
+data_dir = f'./inputs/Location{site_index}.csv'
 data=src.load_data(data_dir)
-print(data)
+
+# Plotting the wind speed data
+src.plot_data( y_data_input=data['windspeed_100m'],
+               site_index=site_index,
+               start_time='2020-01-01 00:00:00',
+               end_time='2020-01-31 00:00:00',
+               title=f'Wind Speed January 2020 at Location {site_index}',
+               xlabel='Time',
+               ylabel='Wind Speed [m/s]',
+               label_legend='Wind Speed at 100m Height'
+               )
--- src/__init__.py
+++ src/__init__.py
@@ -1,5 +1,5 @@
 "Init file containing functions for final project"
-
+import matplotlib.pyplot as plt
 import pandas as pd
 import numpy as np
 
@@ -24,6 +24,58 @@ def load_data(data_path):
     return data
 
 
+def plot_data(y_data_input,
+              site_index,
+              start_time,
+              end_time,
+              title='Data Plot',
+              xlabel='Time',
+              ylabel='Value',
+              label_legend='Data'
+              ):
+    """
+    Plot the data using matplotlib.
+    
+    Parameters
+    ----------
+    x_data_input : pandas.Series
+        The data to plot on x-axis.
+    y_data_input : pandas.Series
+        The data to plot on y-axis.
+    site_index : int
+        The index of the site to plot.
+    start_time : str
+        Start time for the plot in 'YYYY-MM-DD HH:MM:SS' format.
+    end_time : str
+        End time for the plot in 'YYYY-MM-DD HH:MM:SS' format.
+    title : str, optional
+        Title of the plot (default is 'Data Plot').
+    xlabel : str, optional
+        Label for the x-axis (default is 'Time').
+    ylabel : str, optional
+            Label for the y-axis (default is 'Value').
+    
+            
+    Returns
+    -------
+    None
+    """
+   # data_to_plot = load_data(f'./inputs/Location{site_index}.csv')
+    x_data = y_data_input.loc[start_time:end_time].index
+    y_data = y_data_input.loc[start_time:end_time].values
+
+    plt.figure(figsize=(8, 4))
+    plt.plot(x_data, y_data,label=label_legend)
+    plt.title(title,fontsize=16)
+    plt.xlabel(xlabel,fontsize=12)
+    plt.xticks(rotation=45, fontsize=10)
+    plt.xlim(pd.to_datetime(start_time), pd.to_datetime(end_time))
+    plt.ylabel(ylabel,fontsize=12)
+    #plt.ylim(np.min(y_data), np.max(y_data))
+    plt.legend(fontsize=10)
+    plt.grid()
+    plt.show()
+    
 
 
 
