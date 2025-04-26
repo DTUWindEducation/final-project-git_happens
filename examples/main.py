85d811786e852472aea4d1ca2dbec0075e109d3d
s250675 <s250675@dtu.dk>
2025-04-26 15:57:18 +0200

    Added call for lagged_features and split_data functions

--- examples/main.py
+++ examples/main.py
@@ -6,10 +6,25 @@ import src as src
 
 # Can be modified for the different sites
 site_index = 3
+train_size=0.8
+test_size=0.2
 
 # Load the data for the specified site
 data_dir = f'./inputs/Location{site_index}.csv'
-data=src.load_data(data_dir)
+WindData = src.WindFarmDataset(data_dir)
+data=WindData.load_data()
+print(data.head())      # print data head
+
+# Print statistics of the data
+summary = WindData.summary()
+print(summary)
+
+"""
+# Print lenth of training and testing datasets
+train_data, test_data = WindData.split()
+print ('Training Data:', train_data) 
+print ('Test Data:', test_data)
+"""
 
 # Plotting the wind speed data
 src.plot_data( y_data_input=data['windspeed_100m'],
@@ -21,3 +36,13 @@ src.plot_data( y_data_input=data['windspeed_100m'],
                ylabel='Wind Speed [m/s]',
                label_legend='Wind Speed at 100m Height'
                )
+
+#Create lagged features
+lagged_data= WindData.create_lagged_features()
+
+# Split the lagged dataset into training and testing sets
+X_train, X_test, y_train, y_test = src.split_data(lagged_data)
+print('Training Data:', X_train)
+print('Test Data:', X_test)
+print('Training Labels:', y_train)
+print('Test Labels:', y_test)
