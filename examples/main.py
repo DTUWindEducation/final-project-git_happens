d2c86adb7607da00bc9b25ddbb7f57aab731703a
annathuesen <s214253@dtu.dk>
2025-04-28 15:34:36 +0200

    Create classes, fix main

--- examples/main.py
+++ examples/main.py
@@ -4,84 +4,41 @@ from pathlib import Path
 sys.path.append(str(Path(__file__).resolve().parents[1]))
 import src as src 
 
-# Can be modified for the different sites
-site_index = 1
-train_size=0.8
-test_size=0.2
+# Site location and training/testing data size
+site_index = 1      # Change site index to load different locations
+train_size=0.8      # Proportion of data to be used for training
+test_size=0.2       # Proportion of data to be used for testing
 
 # Load the data for the specified site
 data_dir = f'./inputs/Location{site_index}.csv'
 
-WindData = src.WindFarmDataset(data_dir)
-data=WindData.load_data()
-print(data.head())      # print data head
-
-# Print statistics of the data
-summary = WindData.summary()
-print(summary)
-
-"""
-# Print lenth of training and testing datasets
-train_data, test_data = WindData.split()
-print ('Training Data:', train_data) 
-print ('Test Data:', test_data)
-"""
-
+# Load wind data
 WindData = src.WindFarmDataset(data_dir, train_size, test_size)
-data=WindData.load_data()
+data = WindData.load_data()
 print(data.head())      # print data head
 
 # Print statistics of the data
-summary = WindData.summary()
-print(summary)
+# summary = WindData.summary()
+# print(summary)
 
-# Print lenth of training and testing datasets
-split=WindData.split()
-print(len(split[0]), len(split[1]))
+# Split data
+X_train, X_test, y_train, y_test = WindData.split_data()
 
-"""
-# Plotting the wind speed data
-src.plot_data( y_data_input=data['windspeed_100m'],
-               site_index=site_index,
-               start_time='2020-01-01 00:00:00',
-               end_time='2020-01-31 00:00:00',
-               title=f'Wind Speed January 2020 at Location {site_index}',
-               xlabel='Time',
-               ylabel='Wind Speed [m/s]',
-               label_legend='Wind Speed at 100m Height'
-               )
+# Load models
+Model = src.Prediction(X_test, y_test, X_train, y_train)
+y_pred_persistence = Model.persistence_model()
+y_pred_linear_reg = Model.train_linear_regression()
+y_pred_svm = Model.train_svm()
 
-#Create lagged features
-lagged_data= WindData.create_lagged_features()
-
-# Split the lagged dataset into training and testing sets
-X_train, X_test, y_train, y_test = src.split_data(lagged_data)
-#print('Training Data:', X_train)
-#print('Test Data:', X_test)
-#print('Training Labels:', y_train)
-#print('Test Labels:', y_test)
-
-#Persistence Model
-y_pred_persistence = src.persistence_model(y_test)
-#print('Persistence Model Predictions:', y_pred_persistence)
-#print('length of Persistence Model Predictions:', len(y_pred_persistence))
-#print('y_test:', y_test)
-#print('length of y_test:', len(y_test))
-mse_persistence, mae_persistence, rmse_persistence = src.compute_metrics(y_test[1:], y_pred_persistence)
+# Evaluate models
+Evaluation = src.Evaluation(y_test, y_pred_persistence)
+mse_persistence, mae_persistence, rmse_persistence = Evaluation.compute_metrics()
 print(f'Persistence Model - MSE: {mse_persistence}, MAE: {mae_persistence}, RMSE: {rmse_persistence}')
 
-#Linear Regression Model
-y_pred_linear_reg = src.train_linear_regression(X_train, X_test, y_train)
-mse_lin_reg, mae_lin_reg, rmse_lin_reg = src.compute_metrics(y_test, y_pred_linear_reg)
-print(f'Linear Regression Model - MSE: {mse_lin_reg}, MAE: {mae_lin_reg}, RMSE: {rmse_lin_reg}')
+Evaluation = src.Evaluation(y_test, y_pred_linear_reg)
+mse_linear_reg, mae_linear_reg, rmse_linear_reg = Evaluation.compute_metrics()
+print(f'Linear Regression Model - MSE: {mse_linear_reg}, MAE: {mae_linear_reg}, RMSE: {rmse_linear_reg}')
 
-#SVM Model
-y_pred_svm = src.train_svm(X_train, X_test, y_train)
-mse_svm, mae_svm, rmse_svm = src.compute_metrics(y_test, y_pred_svm)
-print(f'SVM Model - MSE: {mse_svm}, MAE: {mae_svm}, RMSE: {rmse_svm}')
-print('Training Data:', X_train)
-print('Test Data:', X_test)
-print('Training Labels:', y_train)
-print('Test Labels:', y_test)
-"""
-
+Evaluation = src.Evaluation(y_test, y_pred_svm)
+rms_svm, mae_svm, mse_svm = Evaluation.compute_metrics()
+print(f'SVM Model - MSE: {mse_svm}, MAE: {mae_svm}, RMSE: {rms_svm}')
--- src/__init__.py
+++ src/__init__.py
@@ -8,8 +8,6 @@ from sklearn import svm
 from sklearn.metrics import mean_squared_error, mean_absolute_error
 
 # --------------- Week 9 ---------------
-class WindFarmDataset:
-    def __init__(self, file_path):
 
 class WindFarmDataset:
     def __init__(self, file_path, train_size=0.8, test_size=0.2):
@@ -35,6 +33,7 @@ class WindFarmDataset:
         """
         return self.data.describe()
 
+    '''
     def split(self):
         """
         Split the dataset into training and testing sets.
@@ -46,81 +45,54 @@ class WindFarmDataset:
         test_data = self.data.iloc[train_size:train_size + test_size]
         
         return train_data, test_data
+    '''
 
-class WindFarmPlotter:
-    def __init__(self, data):
-        """
-        Initialize the Plotter with the dataset.
-        """
-        self.data = data
-
-    def plot_data(self, site_index, start_time, end_time, title='Data Plot', xlabel='Time', ylabel='Value', label_legend='Data'):
-        """
-        Plot the data for a specific site and time range.
-        """
-        x_data = self.data.loc[start_time:end_time].index
-        y_data = self.data.loc[start_time:end_time].values
-
-        plt.figure(figsize=(8, 4))
-        plt.plot(x_data, y_data, label=label_legend)
-        plt.title(title, fontsize=16)
-        plt.xlabel(xlabel, fontsize=12)
-        plt.xticks(rotation=45, fontsize=10)
-        plt.xlim(pd.to_datetime(start_time), pd.to_datetime(end_time))
-        plt.ylabel(ylabel, fontsize=12)
-        plt.legend(fontsize=10)
-        plt.grid()
-        plt.show()
-
-# --------------- Week 8 ---------------
-
-    def load_data(self):
-        """
-        Load the dataset from the specified file path.
-        """
-        self.data = pd.read_csv(self.file_path, parse_dates=['Time'], index_col='Time', sep=',')
-        self.data = self.data.dropna()
-        return self.data
-    
-    def summary(self):
-        """
-        Print a summary of basic statistics for the dataset.
-        """
-        return self.data.describe()
-    
     def create_lagged_features(self):
-        """
-        Creates a new dataset with lagged features based on origin 
-        dataset features (1 hour shift, can be changed later).
-        """
-        
+            """
+            Creates a new dataset with lagged features based on origin 
+            dataset features (1 hour shift, can be changed later).
+            """            
+            if self.data is None:
+                raise ValueError("Data not loaded. Please load the data first.")
+            
+            #self.data['Power_lag1'] = self.data['Power'].shift(1)
+            #self.data['windspeed_100m_lag1'] = self.data['windspeed_100m'].shift(1)
+            
+            for col in self.data.columns: # Iterate through each column in the dataset
+                self.data[f'{col}_lag_1'] = self.data[col].shift(1) # Create lagged features (1 hour shift)
+            
+            self.data = self.data.dropna() # Drop missing values from lag/rolling features
+            
+            return self.data
+
+    def split_data(self):
         if self.data is None:
-            raise ValueError("Data not loaded. Please load the data first.")
+                raise ValueError("Data not loaded. Please load the data first.")
         
-        #self.data['Power_lag1'] = self.data['Power'].shift(1)
-        #self.data['windspeed_100m_lag1'] = self.data['windspeed_100m'].shift(1)
-        
-        for col in self.data.columns: # Iterate through each column in the dataset
-            self.data[f'{col}_lag_1'] = self.data[col].shift(1) # Create lagged features (1 hour shift)
-        
-        self.data = self.data.dropna() # Drop missing values from lag/rolling features
-        
-        return self.data
+        data = self.data.copy()  # Create a copy of the data to avoid modifying the original
+
+        for col in data.columns: # Iterate through each column in the dataset
+                data[f'{col}_lag_1'] = data[col].shift(1) # Create lagged features (1 hour shift)
+            
+        data = data.dropna() # Drop missing values from lag/rolling features
+
+        #Split the lagged dataset into training and testing sets (feature and target).
+        #Does not shuffle time-series data — future data should not influence past predictions.
+        split_idx = int(len(data) * 0.8)  # 80% for training, 20% for testing
     
-    """
-    def split(self):
+        feature_columns = [col for col in data.columns if '_lag_1' in col]  # Select only columns with lagged features
+        X = data[feature_columns]  # Features (lagged variables)
+        y = data['Power']  # 'Power' is the target variable
         
-        Split the dataset into training and testing sets.
+        X_train, X_test = X.iloc[:split_idx], X.iloc[split_idx:]
+        y_train, y_test = y.iloc[:split_idx], y.iloc[split_idx:]
         
-        train_size = int(len(self.data) * self.train_size)
-        test_size = int(len(self.data) * self.test_size)
-        
-        train_data = self.data.iloc[:train_size]
-        test_data = self.data.iloc[train_size:train_size + test_size]
-        
-        return train_data, test_data
-    """
+        return X_train, X_test, y_train, y_test
+
     
+
+        
+
 class WindFarmPlotter:
     def __init__(self, data):
         """
@@ -146,165 +118,80 @@ class WindFarmPlotter:
         plt.grid()
         plt.show()
 
-# --------------- Week 8 ---------------
-"""
-def load_data(data_path):
-    
-    Load data from a file and return it as a numpy array.
-    
-    Parameters
-    ----------
-    data_path : str
-        Path to the data file.
-    
-    Returns
-    -------
-    numpy.ndarray
-        Data from the file.
+class Evaluation:
+    def __init__(self,y_true, y_pred):
+        """
+        Initialize the Evaluation class with true and predicted values.
+        """
+        self.y_true = y_true
+        self.y_pred = y_pred
 
-    data = pd.read_csv(data_path, parse_dates=['Time'], index_col='Time',sep=',') #reading the data
-    data = data.dropna() #dropping the missing values
-
-    return data
-"""
-
-def plot_data(y_data_input,
-              site_index,
-              start_time,
-              end_time,
-              title='Data Plot',
-              xlabel='Time',
-              ylabel='Value',
-              label_legend='Data'
-              ):
-    """
-    Plot the data using matplotlib.
+    # ---Compute Metrics---
+    def compute_metrics(self):
+        """
+        Compute and return the evaluation metrics.
     
-    Parameters
-    ----------
-    x_data_input : pandas.Series
-        The data to plot on x-axis.
-    y_data_input : pandas.Series
-        The data to plot on y-axis.
-    site_index : int
-        The index of the site to plot.
-    start_time : str
-        Start time for the plot in 'YYYY-MM-DD HH:MM:SS' format.
-    end_time : str
-        End time for the plot in 'YYYY-MM-DD HH:MM:SS' format.
-    title : str, optional
-        Title of the plot (default is 'Data Plot').
-    xlabel : str, optional
-        Label for the x-axis (default is 'Time').
-    ylabel : str, optional
-            Label for the y-axis (default is 'Value').
-    
-            
-    Returns
-    -------
-    None
-    """
-   # data_to_plot = load_data(f'./inputs/Location{site_index}.csv')
-    x_data = y_data_input.loc[start_time:end_time].index
-    y_data = y_data_input.loc[start_time:end_time].values
-
-    plt.figure(figsize=(8, 4))
-    plt.plot(x_data, y_data,label=label_legend)
-    plt.title(title,fontsize=16)
-    plt.xlabel(xlabel,fontsize=12)
-    plt.xticks(rotation=45, fontsize=10)
-    plt.xlim(pd.to_datetime(start_time), pd.to_datetime(end_time))
-    plt.ylabel(ylabel,fontsize=12)
-    #plt.ylim(np.min(y_data), np.max(y_data))
-    plt.legend(fontsize=10)
-    plt.grid()
-    plt.show()
-
-
-# --- Split Data --- 
-"""
-def split_data(data):
-    #Split the lagged dataset into training and testing sets (feature and target).
-    feature_columns = [col for col in data.columns if '_lag_1' in col]  # Select only columns with lagged features
-    X = data[feature_columns]  # Features (lagged variables)
-    y = data['Power']  # 'Power' is the target variable
-    
-    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2)
-    
-    return X_train, X_test, y_train, y_test
-"""
-
-def split_data(data):
-    #Split the lagged dataset into training and testing sets (feature and target).
-    #Does not shuffle time-series data — future data should not influence past predictions.
-    split_idx = int(len(data) * 0.8)  # 80% for training, 20% for testing
-    
-    feature_columns = [col for col in data.columns if '_lag_1' in col]  # Select only columns with lagged features
-    X = data[feature_columns]  # Features (lagged variables)
-    y = data['Power']  # 'Power' is the target variable
+        Parameters
+        ----------
+        y_true : numpy.ndarray
+            True values.
+        y_pred : numpy.ndarray
+            Predicted values.
         
-    X_train, X_test = X.iloc[:split_idx], X.iloc[split_idx:]
-    y_train, y_test = y.iloc[:split_idx], y.iloc[split_idx:]
+        Returns
+        -------
+        RMSE, MAE, and MSE.
+        """
+        if len(self.y_true) != len(self.y_pred):
+            self.y_true = self.y_true[1:]  # Shift y_true to match the length of y_pred
+
+        mse = mean_squared_error(self.y_true, self.y_pred)
+        mae = mean_absolute_error(self.y_true, self.y_pred)
+        rmse = np.sqrt(mse)
         
-    return X_train, X_test, y_train, y_test
+        return mse, mae, rmse
 
-# ---Compute Metrics---
-def compute_metrics(y_true, y_pred):
-    """
-    Compute and return the evaluation metrics.
-    
-    Parameters
-    ----------
-    y_true : numpy.ndarray
-        True values.
-    y_pred : numpy.ndarray
-        Predicted values.
-    
-    Returns
-    -------
-    RMSE, MAE, and MSE.
-    """
-    mse = mean_squared_error(y_true, y_pred)
-    mae = mean_absolute_error(y_true, y_pred)
-    rmse = np.sqrt(mse)
-    
-    return mse, mae, rmse
+class Prediction:
+    def __init__(self, X_test, y_test, X_train, y_train):
+        """
+        Initialize the Prediction class with the model and data.
+        """
+        self.X_test = X_test
+        self.y_test = y_test
+        self.X_train = X_train
+        self.y_train = y_train
 
+    # Persistence Model
+    def persistence_model(self):
+        """
+        Predict one-hour ahead power output using persistence model.
+        """
+        y_pred_persistence = self.y_test.shift(1)  # one step persistence (1 hour ahead)
+        y_pred_persistence = y_pred_persistence.dropna()  # Drop the first row with NaN value
+        
+        return y_pred_persistence
 
-#--- Persistence Model ---
-def persistence_model(y_test):
-    """
-    Predict one-hour ahead power output using persistence model.
-    """
-    y_pred_persistence = y_test.shift(1)  # one step persistence (1 hour ahead)
-    y_pred_persistence = y_pred_persistence.dropna()  # Drop the first row with NaN value
-    
-    return y_pred_persistence
+    # Linear Regression Model
+    def train_linear_regression(self):
+        """
+        Train a linear regression model.
+        """
+        model_linear_reg = LinearRegression()
+        model_linear_reg.fit(self.X_train, self.y_train)
+        
+        y_pred_linear_reg = model_linear_reg.predict(self.X_test) # Predict on training data
+        
+        return y_pred_linear_reg
 
-
-# --- Machine Learning Models ---
-# Linear Regression Model
-def train_linear_regression(X_train, X_test, y_train):
-    """
-    Train a linear regression model.
-    """
-    model_linear_reg = LinearRegression()
-    model_linear_reg.fit(X_train, y_train)
-    
-    y_pred_linear_reg = model_linear_reg.predict(X_test) # Predict on training data
-    
-    return y_pred_linear_reg
-
-
-# Support Vector Machine (SVM) Model
-def train_svm(X_train, X_test, y_train):
-    """
-    Train a Support Vector Machine (SVM) model.
-    """    
-    model_svm = svm.SVR()
-    model_svm.fit(X_train, y_train)
-    
-    y_pred_svm = model_svm.predict(X_test)  # Predict on training data
-    
-    return y_pred_svm
+    # Support Vector Machine (SVM) Model
+    def train_svm(self):
+        """
+        Train a Support Vector Machine (SVM) model.
+        """    
+        model_svm = svm.SVR()
+        model_svm.fit(self.X_train, self.y_train)
+        
+        y_pred_svm = model_svm.predict(self.X_test)  # Predict on training data
+        
+        return y_pred_svm
 
