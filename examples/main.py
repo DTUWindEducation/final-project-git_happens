95e38c0e3c3b8bcabdc0aabc1b53303fd14a4e58
s250675 <s250675@dtu.dk>
2025-05-01 23:51:59 +0200

    Added calls for new methods and function in init.py and defined plot timestamps for just 1 week

--- examples/main.py
+++ examples/main.py
@@ -4,32 +4,20 @@ from pathlib import Path
 sys.path.append(str(Path(__file__).resolve().parents[1]))
 import src as src 
 import time
+import os
 
 # Site location and training/testing data size
 #site_index = 1      # Change site index to load different locations
 #train_size=0.8      # Proportion of data to be used for training
 #test_size=0.2       # Proportion of data to be used for testing
 
-def get_valid_site_index():
-    valid_indices = [1, 2, 3, 4]
-
-    while True:
-        try:
-            site_index = int(input("Enter the desired location number (1, 2, 3, or 4): "))
-            if site_index in valid_indices:
-                return site_index
-            else:
-                print("❌ Invalid input. Please enter a valid value for the location (1, 2, 3, or 4).")
-        except ValueError:
-            print("❌ Invalid input. Please enter a number.")
 
 # Example usage
 if __name__ == "__main__":
     print("Welcome to the Wind Farm Prediction System!")
     print("This program will help you predict wind farm power generation.")
 
-
-    site_index = get_valid_site_index()
+    site_index = src.get_valid_site_index()
     print(f"✅ You selected location: {site_index}")
     print("⏱️ Timer has been started...")
     start_time = time.time()  # Start the timer
@@ -41,8 +29,10 @@ if __name__ == "__main__":
     # Load wind data
     WindData = src.WindFarmDataset(data_dir)  #1, train_size, test_size)
     data = WindData.load_data()
+    WindData.transform_wind_directions()  # Transform wind direction to radians
     #print(data.head())      # print data head
 
+    print(data)
     # Plot windspeedat100m
     wind_speed_plotter = src.WindFarmPlotter(data)
     print("📊 Plotting windspeed at 100m...")
@@ -60,12 +50,15 @@ if __name__ == "__main__":
     # summary = WindData.summary()
     # print(summary)
 
+    # Get lag hours from the user
+    lag_hours = src.get_lag_hours()
+    
     # Split data
-    X_train, X_test, y_train, y_test = WindData.split_data()
+    X_train, X_test, y_train, y_test = WindData.split_data(lag_hours)
 
     # Load models
     Model = src.Prediction(X_test, y_test, X_train, y_train)
-    y_pred_persistence = Model.persistence_model()
+    y_pred_persistence = Model.persistence_model(lag_hours)
     y_pred_linear_reg = Model.train_linear_regression()
     y_pred_svm = Model.train_svm()
     y_pred_rf = Model.train_random_forest()
@@ -87,25 +80,18 @@ if __name__ == "__main__":
     mse_rf, mae_rf, rmse_rf = Evaluation.compute_metrics()
     print(f'Random Forest Model - MSE: {mse_rf}, MAE: {mae_rf}, RMSE: {rmse_rf}')
     
-    while True:
-        try:
-            user_month = int(input("Enter the month you want to plot (01-12): "))
-            if 1 <= user_month <= 12:
-                break
-            else:
-                print("❌ Invalid input. Please enter a number between 1 and 12.")
-        except ValueError:
-            print("❌ Invalid input. Please enter a valid number.")
+    # Get the date to plot
+    #plot_date = src.get_plot_date()
         
-    Plotter = src.WindFarmPlotter(data)  # or pass `data` if needed
+    Plotter = src.WindFarmPlotter(data)
     #Plotter.plot_predictions(y_test, y_pred_persistence, y_pred_linear_reg, y_pred_svm)
 
-    Plotter.plot_predictions(y_test, y_pred_persistence, user_month, model_name='Persistence Model')
-    Plotter.plot_predictions(y_test, y_pred_linear_reg, user_month, model_name='Linear Regression')
-    Plotter.plot_predictions(y_test, y_pred_svm, user_month, model_name='SVM')
-    Plotter.plot_predictions(y_test, y_pred_rf, user_month, model_name='Random Forest')
+    Plotter.plot_predictions(y_test, y_pred_persistence, start_time="2021-12-24", end_time="2021-12-31", model_name='Persistence Model', save_path=os.path.join('./outputs/', f'Persistence_Model_Location{site_index}.png'))
+    Plotter.plot_predictions(y_test, y_pred_linear_reg, start_time="2021-12-24", end_time="2021-12-31", model_name='Linear Regression', save_path=os.path.join('./outputs/', f'Linear_Regression_Location{site_index}.png'))
+    Plotter.plot_predictions(y_test, y_pred_svm, start_time="2021-12-24", end_time="2021-12-31", model_name='SVM', save_path=os.path.join('./outputs/', f'SVM_Location{site_index}.png'))
+    Plotter.plot_predictions(y_test, y_pred_rf, start_time="2021-12-24", end_time="2021-12-31", model_name='Random Forest', save_path=os.path.join('./outputs/', f'Random_Forest_Location{site_index}.png'))
 
-    print("📊 Generated plots are saved under ......")
+    print("📊 Generated plots are saved in the ./output/ directory")
     end_time = time.time()  # End the timer
     elapsed_time = end_time - start_time
     print(f"✅ Script finished in {elapsed_time:.2f} seconds.")
